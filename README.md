4e396409f28aab1506edb3f2c92f42c18514a255
Ben Best <ben@ecoquants.com>
2021-12-01 19:05:08 +0000

    init API with basic Plumber example

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # calcofi-api
 Test application programming interface (API)
+
+
+## Run the API
+
+Run the API in the background from server's RStudio Terminal:
+
+```bash
+Rscript /share/github/calcofi-api/run_api.R &
+```
---
+++ plumber.R
@@ -0,0 +1,40 @@
+# plumber.R
+
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+function(msg="") {
+  list(msg = paste0("The message is: '", msg, "'"))
+}
+
+#* Plot a histogram
+#* @serializer png
+#* @get /plot
+function() {
+  rand <- rnorm(100)
+  hist(rand)
+}
+
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
+#* @post /sum
+function(a, b) {
+  as.numeric(a) + as.numeric(b)
+}
+
+#* redirect to the swagger interface 
+#* @get /
+#* @serializer html
+function(req, res) {
+  res$status <- 303 # redirect
+  res$setHeader("Location", "./__swagger__/")
+  "<html>
+  <head>
+    <meta http-equiv=\"Refresh\" content=\"0; url=./__swagger__/\" />
+  </head>
+  <body>
+    <p>For documentation on this API, please visit <a href=\"http://api.ships4whales.org/__swagger__/\">http://api.ships4whales.org/__swagger__/</a>.</p>
+  </body>
+</html>"
+}
